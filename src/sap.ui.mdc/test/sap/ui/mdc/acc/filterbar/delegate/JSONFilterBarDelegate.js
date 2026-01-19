b2b42eedeac0cb7d5c0023d36c0b5af0540bb4c7
Sebastian Allmann <sebastian.allmann@sap.com>
2026-01-19 14:35:06 +0100

    [INTERNAL] FilterBar: improve acc test page
    - use stable IDs
    - add PropertyInfo for valuehelp-tables
    - add search logic to table delegate
    - remove role=application from test page
    - sync FilterField settings with PropertyInfo
    - don't clone ValueHelp on adapt-filters
    - use ValueHelp if added by adapt-filters (after removed)
    
    Change-Id: I15e8a84b25f410023d47d13029dbb2582207e821

--- src/sap.ui.mdc/test/sap/ui/mdc/acc/filterbar/delegate/JSONFilterBarDelegate.js
+++ src/sap.ui.mdc/test/sap/ui/mdc/acc/filterbar/delegate/JSONFilterBarDelegate.js
@@ -4,32 +4,54 @@ sap.ui.define([
 	"sap/ui/mdc/FilterBarDelegate",
 	"../model/metadata/JSONPropertyInfo",
 	"sap/ui/mdc/FilterField",
-	"sap/ui/core/Fragment"
-], function (Element, FilterBarDelegate, JSONPropertyInfo, FilterField, Fragment) {
+	"sap/ui/mdc/odata/v4/TypeMap", // as for Date the V4 type is used
+	"sap/ui/core/Fragment",
+	"sap/ui/core/mvc/View"
+], function (Element, FilterBarDelegate, JSONPropertyInfo, FilterField, ODataV4TypeMap, Fragment, View) {
 	"use strict";
 
 	const JSONFilterBarDelegate = Object.assign({}, FilterBarDelegate);
 
-	JSONFilterBarDelegate.fetchProperties = async () => JSONPropertyInfo;
+	JSONFilterBarDelegate.getTypeMap = function (oPayload) {
+		return ODataV4TypeMap;
+	};
+
+	JSONFilterBarDelegate.fetchProperties = async (oFilterBar) => {
+		return JSONPropertyInfo.mountains;
+	};
 
 	const _createFilterField = async (sId, oProperty, oFilterBar) => {
-		const sPropertyName = oProperty.key;
+		const sPropertyKey = oProperty.key;
 		const oFilterField = new FilterField(sId, {
 			dataType: oProperty.dataType,
 			dataTypeFormatOptions: oProperty.formatOptions,
 			dataTypeConstraints: oProperty.constraints,
-			conditions: "{$filters>/conditions/" + sPropertyName + '}',
-			propertyKey: sPropertyName,
+			conditions: "{$filters>/conditions/" + sPropertyKey + '}',
+			propertyKey: sPropertyKey,
 			required: oProperty.required,
 			label: oProperty.label,
 			maxConditions: oProperty.maxConditions,
-			delegate: {name: "sap/ui/mdc/field/FieldBaseDelegate", payload: {}}
+			delegate: oProperty.dataType?.startsWith("sap.ui.model.odata") ? {name: "delegates/odata/v4/FieldBaseDelegate", payload: {}} : {name: "sap/ui/mdc/field/FieldBaseDelegate", payload: {}}
 		});
+		if (oProperty.dataType?.startsWith("sap.ui.model.odata")) {
+			oFilterField.setOperators(["EQ", "BT", "TODAY", "YESTERDAY", "TOMORROW", "TODAYFROMTO", "LASTDAYS", "NEXTDAYS"]);
+		} else if (sPropertyKey === "rank") {
+			oFilterField.setOperators(["EQ"]);
+		} else if (sPropertyKey === "name") {
+			const oView = getViewForControl(oFilterBar);
+			oFilterField.setValueHelp(oView.createId("VHName"));
+		} else if (sPropertyKey === "countries") {
+			const oView = getViewForControl(oFilterBar);
+			oFilterField.setValueHelp(oView.createId("VHCountries"));
+		} else if (sPropertyKey === "range") {
+			const oView = getViewForControl(oFilterBar);
+			oFilterField.setValueHelp(oView.createId("VHRange"));
+		}
 		return oFilterField;
 	};
 
 	JSONFilterBarDelegate.addItem = async (oFilterBar, sPropertyName) => {
-		const oProperty = JSONPropertyInfo.find((oPI) => oPI.key === sPropertyName);
+		const oProperty = JSONPropertyInfo.mountains.find((oPI) => oPI.key === sPropertyName);
 		const sId = oFilterBar.getId() + "--filter--" + sPropertyName;
 		return Element.getElementById(sId) ?? (await _createFilterField(sId, oProperty, oFilterBar));
 	};
@@ -38,6 +60,17 @@ sap.ui.define([
 		oFilterField.destroy();
 		return true; // allow default handling
 	};
+
+	function getViewForControl(oControl) {
+		if (oControl instanceof View) {
+			return oControl;
+		}
+		if (oControl && typeof oControl.getParent === "function") {
+			oControl = oControl.getParent();
+			return getViewForControl(oControl);
+		}
+		return undefined;
+	}
 
 	return JSONFilterBarDelegate;
 }, /* bExport= */false);
