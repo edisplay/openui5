5784f5747b1ac143ad82ba6ca7973a2bc026dd24
Iliana Bobeva <iliana.bobeva@sap.com>
2026-01-30 11:08:04 +0200

    [FIX] sap.uxap.ObjectPageLayout: fix back navigation to IconTabHeader
    Problem: When there is not enough space for all tabs of IconTabHeader to
    be shown, and user expands the "more" Popover and selects a Section from
    it, when back navigating with shift + tab/shift + F6 through the
    Sections, the focus never gets back to the IconTabHeader (it cycles
    through Sections).
    
    Solution: The problem happens because when IconTabHeader loses focus,
    the last selected tab (Section from the "more" Popover) stays with
    tabindex="0", but all of the other tabs have tabindex="-1" (meaning that
    they can be focused only programatically). When user navigates back and
    reaches the first Section, the only one possible for user focus is the
    one, which is in the Popover. That is why it cannot receive focus.
    
    We need to update tabindex values of all tabs of IconTabHeader in order
    to make the selected tab focusable. That's why, upon selection of a tab,
    now we set its tabindex to "0". All of the others remain with tabindex
    "-1".
    
    SNOW: DINC0594014
    Change-Id: Ifa52fac6337254a5282a08b11b28db0958f116fc

--- src/sap.uxap/test/sap/uxap/qunit/ObjectPageNavigationBar.qunit.js
+++ src/sap.uxap/test/sap/uxap/qunit/ObjectPageNavigationBar.qunit.js
@@ -53,4 +53,68 @@ sap.ui.define([
 		sActualSectionTitle = oObjectPage._oABHelper._getAnchorBar().getItems()[0].getText();
 		assert.strictEqual(sActualSectionTitle, sExpectedSectionTitle, "Anchor bar item title is correct (subsection title)");
 	});
+
+	QUnit.module("Anchor tab focus", {
+		beforeEach: async function() {
+			this.NUMBER_OF_SECTIONS = 3;
+			this.NUMBER_OF_SUB_SECTIONS = 1;
+			this.oObjectPage = utils.helpers.generateObjectPageWithSubSectionContent(utils.oFactory, this.NUMBER_OF_SECTIONS, this.NUMBER_OF_SUB_SECTIONS);
+			this.oObjectPage.placeAt("qunit-fixture");
+			await nextUIUpdate();
+		},
+		afterEach: function() {
+			this.oObjectPage.destroy();
+			this.oObjectPage = null;
+		}
+	});
+
+	QUnit.test("Anchor buttons tabindex values are set correctly when section is selected", function(assert) {
+		// Arrange
+		var oAnchorBar = this.oObjectPage._oABHelper._getAnchorBar(),
+			aAnchorBarItems = oAnchorBar.getItems(),
+			oFirstSection = this.oObjectPage.getSections()[0],
+			oThirdSection = this.oObjectPage.getSections()[2],
+			sFirstSectionId = oFirstSection.getId(),
+			sThirdSectionId = oThirdSection.getId(),
+			oABHelper = this.oObjectPage._oABHelper;
+
+		// Act - select first section
+		oABHelper.selectAnchorForSection(sFirstSectionId);
+
+		// Assert - first anchor should have tabindex 0, others should have -1
+		aAnchorBarItems.forEach(function (oItem) {
+			if (oItem.getKey() === sFirstSectionId) {
+				assert.strictEqual(oItem.$().attr("tabindex"), "0", "Selected anchor button has tabindex 0");
+			} else {
+				assert.strictEqual(oItem.$().attr("tabindex"), "-1", "Non-selected anchor button has tabindex -1");
+			}
+		});
+
+		// Act - select third section
+		oABHelper.selectAnchorForSection(sThirdSectionId);
+
+		// Assert - third anchor should have tabindex 0, others should have -1
+		aAnchorBarItems.forEach(function (oItem) {
+			if (oItem.getKey() === sThirdSectionId) {
+				assert.strictEqual(oItem.$().attr("tabindex"), "0", "Selected anchor button has tabindex 0 after selection change");
+			} else {
+				assert.strictEqual(oItem.$().attr("tabindex"), "-1", "Non-selected anchor button has tabindex -1 after selection change");
+			}
+		});
+	});
+
+	QUnit.test("_setAnchorButtonsTabFocusValues is called when selecting anchor for section", function(assert) {
+		// Arrange
+		var oABHelper = this.oObjectPage._oABHelper,
+			oFirstSection = this.oObjectPage.getSections()[0],
+			sFirstSectionId = oFirstSection.getId(),
+			oSetTabFocusSpy = this.spy(oABHelper, "_setAnchorButtonsTabFocusValues");
+
+		// Act
+		oABHelper.selectAnchorForSection(sFirstSectionId);
+
+		// Assert
+		assert.strictEqual(oSetTabFocusSpy.callCount, 1, "_setAnchorButtonsTabFocusValues is called once");
+		assert.strictEqual(oSetTabFocusSpy.firstCall.args[0], sFirstSectionId, "_setAnchorButtonsTabFocusValues is called with the correct section ID");
+	});
 });
