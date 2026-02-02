80ff0bc8900a337f948a2412d807a41e5009d947
Frank Weigel <frank.weigel@sap.com>
2026-02-02 20:56:48 +0100

    [INTERNAL] sap.ui.core (test): modernize tests for sinon-qunit-bridge
     - migrate to test starter
     - instead of using `QUnit.todo`, intercept `QUnit.pushFailure`
     - add test for private sinon / qunit versions
     - deprecate tests for QUnit 1
    
    Change-Id: I2a7e836bb67197589da1c326e0634fd4aadede92

--- src/sap.ui.core/test/sap/ui/core/qunit/internal/SinonQunitBridge.qunit.js
+++ src/sap.ui.core/test/sap/ui/core/qunit/internal/SinonQunitBridge.qunit.js
@@ -2,18 +2,15 @@
  * ${copyright}
  */
 
-(function () {
+sap.ui.define(function () {
 	"use strict";
-	/*global foo, QUnit, sinon */
+	/*global QUnit, sinon */
 
-	// global test object which is mocked in each test to ensure that verifyAndRestore must run
-	window.foo = {
+	// test object which is mocked in each test to ensure that verifyAndRestore must run
+	const foo = {
 		bar : function () {},
 		baz : function () {}
 	};
-
-	// QUnit.todo does not exist in QUnit 1. Use QUnit.test instead -> the errors will be visible.
-	QUnit.todo = QUnit.todo || QUnit.test;
 
 	function afterEach() {
 		sinon.assert.pass("pass");
@@ -30,6 +27,57 @@
 
 	function useMock(assert) {
 		assert.strictEqual(foo.bar("baz"), 42, "use mock");
+	}
+
+	/**
+	 * Helper to handle expected QUnit failures.
+	 *
+	 * Two tests check that the bridge does not stifle errors in the afterEach phase.
+	 * They throw an error or reject a promise and expect that QUnit reports this as a test failure.
+	 * Normally, this would be a use case for `QUnit.todo` (as `todo` expects at least one error),
+	 * but our infrastructure handles `todo` like failures.
+	 *
+	 * Therefore, the error reporting of QUnit (`pushFailure`) is intercepted and if the expected
+	 * error occurs, it is converted into a successful assertion.
+	 *
+	 * As a safeguard, if no failure is reported, another error is reported.
+	 */
+	function expectOneFailure(rExpectedMessage) {
+		const current = QUnit.config.current;
+		current.pushFailure.restore?.();
+		const pushFailure = current.pushFailure;
+		current.pushFailure = function(message) {
+			// restore original
+			current.pushFailure.restore();
+
+			// convert only the expected error to a successful assertion
+			if (rExpectedMessage.test(message)) {
+				const oResult = {
+					result: true,
+					message,
+					actual: message,
+					expected: rExpectedMessage.source
+				};
+				if ( typeof this.pushResult === "function" ) {
+					return this.pushResult(oResult);
+				} else {
+					// fallback for QUnit < 1.22.0
+					return this.push(
+						oResult.result,
+						oResult.actual,
+						oResult.expected,
+						oResult.message
+					);
+				}
+			}
+
+			// other failures are reported as such
+			return pushFailure.apply(this, arguments);
+		};
+		current.pushFailure.restore = function() {
+			current.pushFailure = pushFailure;
+			return true;
+		};
 	}
 
 	// QUnit.module("nested 1", function () {});
@@ -66,14 +114,21 @@
 	QUnit.module("afterEach throws error", {
 		beforeEach : function () {
 			this.mock(foo).expects("bar").withExactArgs("baz").returns(42);
+			expectOneFailure(/afterEach failed on test: afterEach failed intentionally/);
 		},
 		afterEach : function () {
 			throw new Error("afterEach failed intentionally");
+		},
+		after(context) {
+			// safeguard: fail if no failure was reported
+			if (QUnit.config.current.pushFailure.restore?.()) {
+				context.test.assert.ok(false, "no failure reported");
+			}
 		}
 	});
 
 	// This test MUST report the failure 'afterEach failed on test: afterEach failed intentionally'
-	QUnit.todo("test", useMock);
+	QUnit.test("test", useMock);
 
 	QUnit.module("afterEach returns Promise", {
 		beforeEach : function () {
@@ -94,6 +149,7 @@
 	QUnit.module("afterEach Promise fails", {
 		beforeEach : function () {
 			this.mock(foo).expects("bar").withExactArgs("baz").returns(42);
+			expectOneFailure(/Promise rejected after "?test"?: afterEach Promise rejected intentionally/);
 		},
 		afterEach : function () {
 			return new Promise(function (resolve, reject) {
@@ -101,16 +157,22 @@
 					reject(new Error("afterEach Promise rejected intentionally"));
 				}, 0);
 			});
+		},
+		after(context) {
+			// safeguard: fail if no failure was reported
+			if (QUnit.config.current.pushFailure.restore?.()) {
+				context.test.assert.ok(false, "no failure reported");
+			}
 		}
 	});
 
 	// This test MUST report the failure 'Promise rejected after "test": afterEach Promise rejected
 	// intentionally'
-	QUnit.todo("test", useMock);
+	QUnit.test("test", useMock);
 
 	QUnit.module("beforeEach/afterEach again", oHooks);
 
 	QUnit.test("test1", useMock);
 
 	QUnit.test("test2", useMock);
-}());
+});
