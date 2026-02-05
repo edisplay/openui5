edisplay/openui5

commit 46328b7b3bb27984cffcf49bc3c48405d5afb14d

[FEATURE] sap.uxap.ObjectPageLayout: Action buttons in OverflowToolbar
* All buttons in the "actions" aggregation of ObjectPageSubSection are now placed in an OverflowToolbar.

* For the titles of Sections/SubSections now Title control is used with the respective configuration for titleStyle and level (level can be further configured by applications, according to the ACC needs).

* When a SubSection is promoted (it's the only one in a particular Section and has no title), properties of the Section's title are propagated to the promoted SubSection's title, so that we can just show the SubSection header with its OverflowToolbar and SubSection title (looking like a Section title).

* Title of the first Section is now shown by default (as per the new VD
  spec). If applications want to align with the old UX spec, they can
  use the showTitle property set to "false" to hide the title of the
  first Section.

* Visual updates according to the new VD spec (mainly heights of Section/SubSection headers and paddings).

JIRA: BGSOFUIPIRIN-6627
Change-Id: Ibeb4803d0609c53b747fe8577f9edd8da322d542

src/sap.uxap/src/sap/uxap/ObjectPageLayout.js
@@ -1719,8 +1719,7 @@ sap.ui.define([
 	 */
 	ObjectPageLayout.prototype._applyUxRules = function (bInvalidate) {
 		var aSections, aSubSections, iVisibleSubSections, iVisibleSection, iVisibleBlocks,
-			bVisibleAnchorBar, bUseIconTabBar, oFirstVisibleSection, oFirstVisibleSubSection,
-			bFirstSectionTitleHidden, aContent, iFirstVisibleSectionVisibleSubSections, oTitleVisibilityInfo = {};
+			bVisibleAnchorBar, bUseIconTabBar, oFirstVisibleSection, oFirstVisibleSubSection, oTitleVisibilityInfo = {};
 
 		aSections = this.getSections() || [];
 		iVisibleSection = 0;
@@ -1738,10 +1737,15 @@ sap.ui.define([
 				return true;
 			}
 
+			oSection.removeStyleClass("sapUxAPObjectPageSectionFirstVisible");
 			this._registerSectionBaseInfo(oSection);
 			aSubSections = oSection.getSubSections() || [];
 			iVisibleSubSections = 0;
 			oFirstVisibleSubSection = null;
+
+			if (this._shouldApplySectionTitleLevel(oSection)) {
+				oSection._setInternalTitleLevel(this._determineSectionBaseInternalTitleLevel(oSection), bInvalidate);
+			}
 
 			aSubSections.forEach(function (oSubSection) {
 
@@ -1759,23 +1763,22 @@ sap.ui.define([
 					Log.info("ObjectPageLayout :: noVisibleBlock UX rule matched", "subSection " + oSubSection.getTitle() + " forced to hidden");
 				} else {
 					oSubSection._setInternalVisible(true, bInvalidate);
-					//if TitleOnTop.sectionGetSingleSubSectionTitle is matched, this will be hidden back
-					oTitleVisibilityInfo[oSubSection.getId()] = true;
 					iVisibleSubSections++;
 					if (!oFirstVisibleSubSection) {
 						oFirstVisibleSubSection = oSubSection;
 					}
 
 					if (this._shouldApplySectionTitleLevel(oSubSection)) {
-						oSubSection._setInternalTitleLevel(this._determineSectionBaseInternalTitleLevel(oSubSection, !oFirstVisibleSection), bInvalidate);
+						oSubSection._setInternalTitleLevel(this._determineSectionBaseInternalTitleLevel(oSubSection), bInvalidate);
 					}
 				}
 
 			}, this);
 
 			if (iVisibleSubSections > 1) {
+				oSection._setHasPromotedSubSection(false, bInvalidate);
 				aSubSections.forEach(function(oSubSection) {
-					oSubSection._setBorrowedTitleDomId("");
+					oSubSection._setIsPromoted(false, bInvalidate);
 				});
 			}
 
@@ -1788,28 +1791,20 @@ sap.ui.define([
 				oTitleVisibilityInfo[oSection.getId()] = true;
 				if (!oFirstVisibleSection) {
 					oFirstVisibleSection = oSection;
-					iFirstVisibleSectionVisibleSubSections = iVisibleSubSections;
+					oFirstVisibleSection.addStyleClass("sapUxAPObjectPageSectionFirstVisible");
 				}
 
-				//rule TitleOnTop.sectionGetSingleSubSectionTitle: If a section as only 1 subsection and the subsection title is not empty, the section takes the subsection title on titleOnTop layout only
-				if (this.getSubSectionLayout() === ObjectPageSubSectionLayout.TitleOnTop &&
-					iVisibleSubSections === 1 && oFirstVisibleSubSection.getTitle().trim() !== "") {
-					Log.info("ObjectPageLayout :: TitleOnTop.sectionGetSingleSubSectionTitle UX rule matched", "section " + oSection.getTitle() + " is taking its single subsection title " + oFirstVisibleSubSection.getTitle());
-					oSection._setInternalTitle(oFirstVisibleSubSection.getTitle(), bInvalidate);
-					oTitleVisibilityInfo[oFirstVisibleSubSection.getId()] = false;
+				var bHasPromotedSubSection = this.getSubSectionLayout() === ObjectPageSubSectionLayout.TitleOnTop &&
+					iVisibleSubSections === 1 && oFirstVisibleSubSection.getTitle().trim() !== "";
 
-					// Title propagation support - set the borrowed Dom ID to the section title
-					oFirstVisibleSubSection._setBorrowedTitleDomId(oSection.getId() + "-title");
-				} else {
-					oSection._setInternalTitle("", bInvalidate);
+				//rule TitleOnTop.sectionGetSingleSubSectionTitle: If a section as only 1 subsection and the subsection title is not empty, the SubSection takes the Section's title level with titleOnTop layout only
+				if (bHasPromotedSubSection) {
+					oFirstVisibleSubSection._setIsPromoted(true, bInvalidate);
+					oSection._setHasPromotedSubSection(true, bInvalidate);
 				}
 
 				if (iVisibleSubSections === 1 && !oFirstVisibleSubSection.getTitle().trim()) {
 					oFirstVisibleSubSection._setBorrowedTitleDomId(oSection.getId() + "-title");
-				}
-
-				if (this._shouldApplySectionTitleLevel(oSection)) {
-					oSection._setInternalTitleLevel(this._determineSectionBaseInternalTitleLevel(oSection), bInvalidate);
 				}
 
 				iVisibleSection++;
@@ -1817,6 +1812,7 @@ sap.ui.define([
 
 			if (bUseIconTabBar) {
 				oTitleVisibilityInfo[oSection.getId()] = false;
+				oSection.addStyleClass("sapUxAPObjectPageSectionFirstVisible");
 			}
 		}, this);
 
@@ -1828,10 +1824,6 @@ sap.ui.define([
 			if (bUseIconTabBar && oFirstVisibleSection) {
 				oTitleVisibilityInfo[oFirstVisibleSection.getId()] = true;
 			}
-		} else if (oFirstVisibleSection && bVisibleAnchorBar) {
-			bFirstSectionTitleHidden = true;
-			oTitleVisibilityInfo[oFirstVisibleSection.getId()] = false;
-			Log.info("ObjectPageLayout :: firstSectionTitleHidden UX rule matched", "section " + oFirstVisibleSection.getTitle() + " title forced to hidden");
 		}
 
 		this.toggleStyleClass(ObjectPageLayout.NO_NAVIGATION_CLASS_NAME, iVisibleSection <= 1);
@@ -1851,14 +1843,6 @@ sap.ui.define([
 		this._bAllContentFitsContainer = (iVisibleSection === 1)
 			&& (iVisibleSubSections === 1)
 			&& this._oFirstVisibleSubSection.hasStyleClass(ObjectPageSubSection.FIT_CONTAINER_CLASS);
-
-		if (bFirstSectionTitleHidden && (iFirstVisibleSectionVisibleSubSections === 1)) {
-			// Title propagation support - set the borrowed title Dom ID to the first AnchorBar button
-			aContent = this.getAggregation("_anchorBar").getItems();
-			if (aContent.length) {
-				this._oFirstVisibleSubSection._setBorrowedTitleDomId(aContent[0].getId() + "-content");
-			}
-		}
 
 		this._checkSubSectionVisibilityChange();
 	};
@@ -2678,7 +2662,7 @@ sap.ui.define([
 
 			if (!oInfo.isSection && (oSection = oSectionBase.getParent())) {
 				// a promoted subSection borrows the title of its parent section
-				bPromoted = oSectionBase._getTitleDomId() === oSection.getId() + "-title";
+				bPromoted = oSectionBase._isPromoted();
 				if (bPromoted) {
 					// the scrollTop required to scroll to a promoted subsection
 					// is the top of the parent section (UX rule)
@@ -2690,8 +2674,6 @@ sap.ui.define([
 			if (!this._bStickyAnchorBar && !this._bHeaderInTitleArea) { // in sticky mode the anchor bar is not part of the content
 				oInfo.positionTop -= this.iAnchorBarHeight;
 			}
-
-			oInfo.sectionReference.toggleStyleClass("sapUxAPObjectPageSubSectionPromoted", bPromoted);
 
 			//for calculating the currently scrolled section of subsection (and for lazy loading) we also need to know the bottom of the section and subsections
 			//we can't use oInfo.$dom.height() since the margin are not taken into account.
@@ -2926,22 +2908,20 @@ sap.ui.define([
 	 * <code>sap.ui.core.TitleLevel.H3</code> is returned for <code>ObjectPageSection</code> and
 	 * <code>sap.ui.core.TitleLevel.H4</code> for <code>ObjectPageSubSection</code>.
 	 * @param {sap.uxap.ObjectPageSectionBase} oSectionBase <code>ObjectPageSectionBase</code> instance
-	 * @param {boolean} bFirstVisibleSection if the section is the first visible section (only used when determining subsection title level)
 	 * @returns {string} <code>sap.ui.core.TitleLevel</code>
 	 * @since 1.44
 	 * @private
 	 */
-	ObjectPageLayout.prototype._determineSectionBaseInternalTitleLevel = function(oSectionBase, bFirstVisibleSection) {
+	ObjectPageLayout.prototype._determineSectionBaseInternalTitleLevel = function(oSectionBase) {
 		var sSectionBaseTitleLevel = this.getSectionTitleLevel(),
 			bIsSection = oSectionBase instanceof ObjectPageSection;
 
 		if (sSectionBaseTitleLevel === TitleLevel.Auto) {
-			// if this is a subsection contained inside the first visible section - meaning that the section's title is hidden
-			// then we need to return H3 to prevent heading levels increasing by more than one
-			if (bIsSection || (bFirstVisibleSection && !bIsSection)) {
-				// section or subsections inside the first visible section
+			if (bIsSection) {
+				// Section
 				return TitleLevel.H3;
 			}
+			// SubSection
 			return TitleLevel.H4;
 		}
 
@@ -3100,7 +3080,7 @@ sap.ui.define([
 		oSectionBase = Element.getElementById(sSectionId);
 
 		bShouldDisplayParentTitle = oSectionBase && oSectionBase instanceof ObjectPageSubSection &&
-			(oSectionBase.getTitle().trim() === "" || !oSectionBase._getInternalTitleVisible() || oSectionBase.getParent()._getIsHidden());
+			(oSectionBase.getTitle().trim() === "" || oSectionBase.getParent()._getIsHidden());
 
 		//the sectionBase title needs to be visible (or the user won't "feel" scrolling that sectionBase but its parent)
 		//see Incident 1570016975 for more details

commit 4b3ef09eb9e861ab52a21803f364167b7431dfdd

[FIX] sap.uxap.ObjectPageLayout: Ensure subsection visibility is tracked correctly
When toggling useIconTabBar multiple times (false ? true ? false), ObjectPageSubSection titles were not being restored to their original visibility state. This regression was caused by commit 46328b7b3bb, which removed the line that registers visible subsections in the oTitleVisibilityInfo tracking object.

The missing registration prevented the _applyUxRules method from correctly processing subsection visibility changes when UX rules were re-evaluated during useIconTabBar toggles. Without this registration, the subsections were not included in the title visibility tracking map, causing the TitleOnTop layout rules to fail in restoring their titles.

This fix restores the critical line that registers each visible subsection in oTitleVisibilityInfo, ensuring the title visibility system can properly apply dynamic layout rules when the navigation mode changes.

SNOW: DINC0702145
Change-Id: Iae06129bf0ec817cb14c21e292c4eee664eb32f1

src/sap.uxap/src/sap/uxap/ObjectPageLayout.js
@@ -1767,6 +1767,8 @@ sap.ui.define([
 					Log.info("ObjectPageLayout :: noVisibleBlock UX rule matched", "subSection " + oSubSection.getTitle() + " forced to hidden");
 				} else {
 					oSubSection._setInternalVisible(true, bInvalidate);
+					//if TitleOnTop.sectionGetSingleSubSectionTitle is matched, this will be hidden back
+					oTitleVisibilityInfo[oSubSection.getId()] = true;
 					iVisibleSubSections++;
 					if (!oFirstVisibleSubSection) {
 						oFirstVisibleSubSection = oSubSection;

src/sap.uxap/test/sap/uxap/qunit/ObjectPageSubSection.qunit.js
@@ -2353,6 +2353,72 @@ function(Element, nextUIUpdate, $, Control, coreLibrary, XMLView, ResizeHandler,
 		this.oObjectPage.destroy();
 	});
 
+	QUnit.test("Subsection _setInternalTitleVisible is called when toggling useIconTabBar (DINC0702145)", function(assert) {
+		// Arrange: Create layout with multiple subsections having titles
+		var oSubSection1 = new ObjectPageSubSectionClass({
+				title: "SubSection 1",
+				blocks: [new Text({text: "content 1"})]
+			}),
+			oSubSection2 = new ObjectPageSubSectionClass({
+				title: "SubSection 2",
+				blocks: [new Text({text: "content 2"})]
+			}),
+			oObjectPage = new ObjectPageLayout({
+				sections: [
+					new ObjectPageSection({
+						title: "Section 1",
+						subSections: [oSubSection1, oSubSection2]
+					})
+				]
+			}),
+			oSpy1,
+			oSpy2,
+			done = assert.async();
+
+		assert.expect(4);
+
+		oObjectPage.placeAt('qunit-fixture');
+
+		oObjectPage.attachEventOnce("onAfterRenderingDOMReady", function() {
+			// Create spies after initial rendering
+			oSpy1 = sinon.spy(oSubSection1, "_setInternalTitleVisible");
+			oSpy2 = sinon.spy(oSubSection2, "_setInternalTitleVisible");
+
+			// Act: Toggle useIconTabBar on - this triggers _adjustLayoutAndUxRules internally
+			oObjectPage.setUseIconTabBar(true);
+			oObjectPage._adjustLayoutAndUxRules();
+
+			// Assert: Verify _setInternalTitleVisible was called on subsections
+			// This would fail if line 1774 (oTitleVisibilityInfo[oSubSection.getId()] = true;) is deleted
+			// because subsections wouldn't be registered in oTitleVisibilityInfo and wouldn't be processed
+			assert.ok(oSpy1.called,
+				"SubSection 1 _setInternalTitleVisible was called after toggling to IconTabBar mode");
+			assert.ok(oSpy2.called,
+				"SubSection 2 _setInternalTitleVisible was called after toggling to IconTabBar mode");
+
+			// Reset spies
+			oSpy1.resetHistory();
+			oSpy2.resetHistory();
+
+			// Act: Toggle useIconTabBar off and adjust UX rules again
+			oObjectPage.setUseIconTabBar(false);
+			oObjectPage._adjustLayoutAndUxRules();
+
+			// Assert: Verify _setInternalTitleVisible was called again after toggling back
+			assert.ok(oSpy1.called,
+				"SubSection 1 _setInternalTitleVisible was called after toggling back from IconTabBar mode");
+			assert.ok(oSpy2.called,
+				"SubSection 2 _setInternalTitleVisible was called after toggling back from IconTabBar mode");
+
+			// Cleanup
+			oSpy1.restore();
+			oSpy2.restore();
+			oObjectPage.destroy();
+
+			done();
+		});
+	});
+
 	QUnit.module("SubSection internalTitle");
 
 	QUnit.test("Subsection _setInternalTitleLevel should invalidate control", async function(assert) {