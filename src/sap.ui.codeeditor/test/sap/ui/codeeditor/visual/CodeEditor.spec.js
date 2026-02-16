ec2823a3810e717b5cd9afbb56268cef7647e37f
Petar Dimov <petar.dimov@sap.com>
2026-02-16 16:13:38 +0200

    [INTERNAL] sap.ui.codeeditor.CodeEditor: Stabilize visual test
    SNOW: DINC0774537
    Change-Id: I89fb67464018454d4553d00a03f2882a572ee037

--- src/sap.ui.codeeditor/test/sap/ui/codeeditor/visual/CodeEditor.spec.js
+++ src/sap.ui.codeeditor/test/sap/ui/codeeditor/visual/CodeEditor.spec.js
@@ -1,9 +1,12 @@
-/* global describe, it, takeScreenshot, expect */
+/* global describe, it, takeScreenshot, expect, browser */
 
 describe("sap.ui.codeeditor.CodeEditor", function() {
 	"use strict";
 
 	it("should see the code editor", function () {
+		// Wait for the editor to be fully rendered
+		browser.sleep(1000);
+
 		expect(takeScreenshot()).toLookAs("0_initial");
 	});
 
