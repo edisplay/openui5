cf69a7b9ed2d6106a1df1300ebd5bfa2790f7bec
I523040 <colin.otchere@sap.com>
2026-04-14 15:39:14 +0200

    [INTERNAL] sap.ui.fl: Add support for lazy loading in LRepConnector
    Add lazyLoadingViewsEnabled parameter to the LRepConnector's loadFlexData to signal to the back end that lazy loading of variants is supported. The parameter is removed when starting key user adaptation.
    
    JIRA: UITECHGRAVITY-5968
    Change-Id: Ic7ac217bc951e773f98c46532135ff545a9c5135

--- src/sap.ui.fl/src/sap/ui/fl/interfaces/BaseLoadConnector.js
+++ src/sap.ui.fl/src/sap/ui/fl/interfaces/BaseLoadConnector.js
@@ -24,6 +24,7 @@ sap.ui.define([], function() {
 		 * @param {string} mPropertyBag.flexReference Reference of the application
 		 * @param {string} [mPropertyBag.url] Configured URL for the connector
 		 * @param {string} [mPropertyBag.cacheKey] Key which can be used to etag / cachebuster the request
+		 * @param {boolean} [mPropertyBag.lazyLoadingViewsEnabled] Signals to the back end that lazy loading of variants is supported, must not be set when starting key user adaptation
 		 * @returns {Promise<Object>} Promise resolving with an object containing a flex data response
 		 *
 		 * @private
@@ -66,6 +67,50 @@ sap.ui.define([], function() {
 		 */
 		loadFlVariant() {
 			return Promise.reject(new Error("loadFlVariant is not implemented"));
+		},
+
+		/**
+		 * Load all FL variants and their metadata for Manage Views dialog.
+		 * @param {object} mPropertyBag Property bag
+		 * @param {string} mPropertyBag.reference Flexibility reference
+		 * @param {string} mPropertyBag.vmReference Id of the control for which the variants should be loaded
+		 * @returns {Promise<object>} Promise resolves with an object containing the flex variants for the given control
+		 */
+		loadAllFLVariants() {
+			return Promise.reject(new Error("loadAllFLVariants is not implemented"));
+		},
+
+		/**
+		 * Load missing UI changes when switching to a variant that has variantDependentControlChangesRemoved: true
+		 * @param {object} mPropertyBag Property bag
+		 * @param {string} mPropertyBag.reference Flexibility reference
+		 * @param {string} mPropertyBag.id Id of the variant for which the content should be loaded
+		 * @returns {Promise<object>} Promise resolves with an object containing the flex variant content for the given variant ID
+		 */
+		loadFlVariantContent() {
+			return Promise.reject(new Error("loadFlVariantContent is not implemented"));
+		},
+
+		/**
+		 * Load all comp variants and their metadata for Manage Views dialog.
+		 * @param {object} mPropertyBag Property bag
+		 * @param {string} mPropertyBag.reference Flexibility reference
+		 * @param {string} mPropertyBag.persistencyKey Persistency key of the control for which the variants should be loaded
+		 * @returns {Promise<object>} Promise resolves with an object containing the flex variants for the given control
+		 */
+		loadAllCompVariants() {
+			return Promise.reject(new Error("loadAllCompVariants is not implemented"));
+		},
+
+		/**
+		 * Load missing content when switching to a variant that has contentRemoved: true.
+		 * @param {object} mPropertyBag Property bag
+		 * @param {string} mPropertyBag.reference Flexibility reference
+		 * @param {string} mPropertyBag.id Id of the variant for which the content should be loaded
+		 * @returns {Promise<object>} Promise resolves with an object containing the flex variant content for the given variant ID
+		 */
+		loadCompVariantContent() {
+			return Promise.reject(new Error("loadCompVariantContent is not implemented"));
 		}
 	};
 
