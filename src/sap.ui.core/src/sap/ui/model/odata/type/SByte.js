59ff0076fef897d7805282b40b86668ee8bbe5b3
viktorsperling <viktor.sperling@sap.com>
2026-04-24 11:33:21 +0200

    [INTERNAL] sap.ui.core.format.NumberFormat: Introduce typedefs for format options to NumberFormat
    PS1: Add the base type for FormatOptions
    PS2: Add typedef for Integer format options
    PS3: Adjust texts in the new typedefs
    PS4: Review own comments
    PS5: Review comments
    PS6: Rebase
    PS7: Add defaults to NumberFormat#getIntegerInstance
    PS8: Review comments
    PS9: Add format options typedef to model.type.Integer
    PS10: Rebase
    PS11: Add format options typedefs to model.odata.type.Int/Int16/Int32/Int64
    PS12: Add format options typedefs to model.odata.type.Byte/SByte
    PS13-14: Review comments
    
    JIRA: CPOUI5MODELS-1111
    Change-Id: I9c8b0ab452955c4ec48d939ca7ca9495ed02d9e9

--- src/sap.ui.core/src/sap/ui/model/odata/type/SByte.js
+++ src/sap.ui.core/src/sap/ui/model/odata/type/SByte.js
@@ -29,12 +29,24 @@ sap.ui.define([
 	 * @version ${version}
 	 *
 	 * @alias sap.ui.model.odata.type.SByte
-	 * @param {object} [oFormatOptions]
+	 * @param {sap.ui.model.odata.type.IntTypeFormatOptions} [oFormatOptions={
+	 *     emptyString: NaN,
+	 *     groupingBaseSize: 3,
+	 *     groupingEnabled: false,
+	 *     groupingSize: 3,
+	 *     maxFractionDigits: 0,
+	 *     maxIntegerDigits: 99,
+	 *     minFractionDigits: 0,
+	 *     minIntegerDigits: 1,
+	 *     parseAsString: false,
+	 *     parseEmptyValueToZero: false,
+	 *     preserveDecimals: false,
+	 *     roundingMode: "TOWARDS_ZERO",
+	 *     showScale: true,
+	 *     strictGroupingValidation: false,
+	 *     style: "standard"
+	 *   }]
 	 *   Format options as defined in {@link sap.ui.core.format.NumberFormat.getIntegerInstance}
-	 * @param {boolean} [oFormatOptions.parseEmptyValueToZero=false]
-	 *   Whether the empty string and <code>null</code> are parsed to <code>0</code> if the <code>nullable</code>
-	 *   constraint is set to <code>false</code>; see {@link sap.ui.model.odata.type.Int#parseValue parseValue};
-	 *   since 1.115.0
 	 * @param {object} [oConstraints]
 	 *   constraints; {@link sap.ui.model.odata.type.Int#validateValue validateValue} throws an
 	 *   error if any constraint is violated
